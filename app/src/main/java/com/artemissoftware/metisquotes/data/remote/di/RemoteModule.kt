c0ec8bde5b9e8ead5aa9b54f6f24e7c981dc56ff
gustavo <gustavo.isaac@gmail.com>
2024-11-12 17:38:29 +0000

    RemoteModule

---
+++ app/src/main/java/com/artemissoftware/metisquotes/data/remote/di/RemoteModule.kt
@@ -0,0 +1,46 @@
+package com.artemissoftware.metisquotes.data.remote.di
+
+import com.artemissoftware.metisquotes.data.remote.api.QuoteApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RemoteModule {
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(
+                HttpLoggingInterceptor()
+                    .setLevel(HttpLoggingInterceptor.Level.BODY),
+            )
+            .readTimeout(15L, TimeUnit.SECONDS)
+            .connectTimeout(15L, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(QuoteApi.BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideQuoteApi(retrofit: Retrofit): QuoteApi {
+        return retrofit.create(QuoteApi::class.java)
+    }
+}
