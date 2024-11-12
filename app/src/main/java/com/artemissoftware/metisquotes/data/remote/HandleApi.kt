7889b8405c517d3edef8f9c99749a3040cd84a85
gustavo <gustavo.isaac@gmail.com>
2024-11-12 09:32:28 +0000

    HandleApi

---
+++ app/src/main/java/com/artemissoftware/metisquotes/data/remote/HandleApi.kt
@@ -0,0 +1,11 @@
+package com.artemissoftware.metisquotes.data.remote
+
+internal object HandleApi {
+    suspend fun <T> safeApiCall(callFunction: suspend () -> T): T {
+        return try {
+            callFunction.invoke()
+        } catch (ex: Exception) {
+            throw ex
+        }
+    }
+}
