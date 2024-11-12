3ca80875f6b98a37811762cba24ee6d5378e8dbd
gustavo <gustavo.isaac@gmail.com>
2024-11-12 09:36:33 +0000

    Resource

---
+++ app/src/main/java/com/artemissoftware/metisquotes/domain/Resource.kt
@@ -0,0 +1,18 @@
+package com.artemissoftware.metisquotes.domain
+
+import com.artemissoftware.metisquotes.domain.error.Error
+
+sealed interface Resource<T> {
+    data class Success<T>(val data: T) : Resource<T>
+    data class Failure<T>(val error: Error) : Resource<T>
+
+    fun onSuccess(block: (T) -> Unit): Resource<T> {
+        if (this is Success) block(data)
+        return this
+    }
+
+    fun onFailure(block: (Error) -> Unit): Resource<T> {
+        if (this is Failure) block(error)
+        return this
+    }
+}
