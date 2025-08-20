1a5797093de1b463c447dbe185b9c76a547ca5e5
gustavo <gustavo.isaac@gmail.com>
2025-08-20 18:16:51 +0100

    RootNavGraph

---
+++ app/src/main/java/com/artemissoftware/metisquotes/presentation/navigation/RootNavGraph.kt
@@ -0,0 +1,31 @@
+package com.artemissoftware.metisquotes.presentation.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.artemissoftware.metisquotes.presentation.dailyquotes.DailyQuotesScreen
+import com.artemissoftware.metisquotes.presentation.randomquotes.RandomQuoteScreen
+
+const val ROOT_GRAPH = "root_graph"
+
+@Composable
+fun RootNavGraph(
+    navController: NavHostController,
+    startDestination: String,
+) {
+    NavHost(
+        navController = navController,
+        route = ROOT_GRAPH,
+        startDestination = startDestination,
+    ) {
+
+        composable(Route.DailyQuotes.route) {
+            DailyQuotesScreen()
+        }
+
+        composable(Route.RandomQuote.route) {
+            RandomQuoteScreen()
+        }
+    }
+}
